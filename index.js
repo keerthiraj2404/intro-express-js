e24156ed804de39df8fb4030fa8931e64e3299c5
= <=>
2023-03-11 04:40:14 +0000

    3 types of examples were mentioned in the index.js file

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (request, response) => {
+  response.send("Hello World!");
+});
+
+app.get("/date", (request, response) => {
+  const newDate = new Date();
+  response.send(`Today's date is ${newDate}`);
+});
+
 app.get("/page", (request, response) => {
   response.sendFile("./page.html", { root: __dirname });
 });
